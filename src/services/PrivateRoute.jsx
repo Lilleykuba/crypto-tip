7909256940ef294591114a6e81e2df5606029014
Camobeast <jakub.lilley@gmail.com>
2024-11-25 16:26:41 +0100

    fix!!!

--- src/services/PrivateRoute.jsx
+++ src/services/PrivateRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../services/AuthContext"; // Ensure correct path to your AuthContext
 
 const PrivateRoute = ({ children }) => {
-  const { authUser } = useAuth(); // Get current user from context
+  const { user } = useAuth(); // Get current user from context
 
   // Redirect to login page if the user is not authenticated
   return user ? children : <Navigate to="/login" />;
