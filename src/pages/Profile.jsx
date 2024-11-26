686d2bfcf866a7f2756ce349aefc7fa348a2ef8a
Camobeast <jakub.lilley@gmail.com>
2024-11-26 17:25:41 +0100

    fix

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 // Profile.jsx
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { QRCodeCanvas } from "qrcode.react";
 import { ethers } from "ethers";
 import { isAddress } from "ethers";
